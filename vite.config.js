d42cea9cce62b448256969027eb585d40c33cd22
hazzma <hansel0883@gmail.com>
2026-05-09 23:30:25 +0700

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mochi-Animator-Online/',
   plugins: [
     react(),
     tailwindcss(),
